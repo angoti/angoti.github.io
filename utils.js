3e08a98450fdc2819891cd46970ef583d76e833f
Edson Angoti Júnior <angoti@gmail.com>
2022-04-22 19:38:54 -0300

    add copy button

--- utils.js
+++ utils.js
@@ -1,14 +1,19 @@
-function copyToClipboard() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
-
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-}
+Array.from(document.getElementsByTagName("code")).forEach(function (container) {
+  let btn = new Image();
+  btn.src = "https://cdn.iconscout.com/icon/free/png-32/copy-data-667851.png";
+  btn.style.float = "right";
+  btn.style.color = "white";
+  btn.onclick = () => {
+    navigator.clipboard.writeText(document.getElementsByTagName("code")[7].innerText).then(
+      () => {
+        alert("ok");
+        return false;
+      },
+      () => {
+        alert("erro");
+        return false;
+      }
+    );
+  };
+  container.insertBefore(btn, container.firstChild);
+});
